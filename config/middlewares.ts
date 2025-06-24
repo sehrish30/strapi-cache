8f8401daaa8b552b8af743bf9414adbd0ca4e6fc
sehrish30 <sehrishwaheed98@gmail.com>
2025-06-24 10:13:05 +0300

    middlewares fix

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,54 +1,9 @@
 export default [
-  "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "script-src": ["'self'", "*.tinymce.com", "*.tiny.cloud", "https:"],
-          "connect-src": [
-            "'self'",
-            "*.tinymce.com",
-            "*.tiny.cloud",
-            "blob:",
-            "*.strapi.io",
-          ],
-          "style-src": [
-            "'self'",
-            "'unsafe-inline'",
-            "*.tinymce.com",
-            "*.tiny.cloud",
-          ],
-          "img-src": [
-            "'self'",
-            "*.tinymce.com",
-            "*.tiny.cloud",
-            "data:",
-            "blob:",
-            "https://market-assets.strapi.io",
-            process.env.STRAPI_ADMIN_BUNNY_PULL_ZONE,
-          ],
-          "font-src": ["'self'", "*.tinymce.com", "*.tiny.cloud"],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            process.env.STRAPI_ADMIN_BUNNY_PULL_ZONE,
-          ],
-        },
-        upgradeInsecureRequests: null,
-      },
-    },
-  },
-  {
-    name: "strapi::poweredBy",
-    config: {
-      poweredBy: "Tap Payments",
-    },
-  },
-  "strapi::cors",
   "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
   "strapi::session",
